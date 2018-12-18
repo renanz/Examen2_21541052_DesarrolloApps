ca829364c87cc3104b75f713acdbf2d3f2e44dcc
renanz <renan_zelaya@hotmail.com>
2018-12-18 17:41:53 -0600

    WIP

--- App.js
+++ App.js
@@ -1,30 +1,78 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button, TextInput, ScrollView } from "react-native";
+import axios from "axios";
 import { createStackNavigator, createAppContainer } from "react-navigation";
 
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {label: '' };
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Home Screen</Text>
+        <Text>Job Search</Text>
+        <TextInput
+            style={styles.textInput}
+            editable = {true}
+            onChangeText={(text) => this.setState({
+              label: text
+            })}
+            placeholder='Search'
+          />
         <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate("Details")}
+          title="Button"
+          onPress={() => this.props.navigation.navigate("ListOfJobs", {search: this.state.label})}
         />
       </View>
     );
   }
 }
 
-class DetailsScreen extends React.Component {
+class ListOfJobs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { jobs: [], search: this.props.navigation.state.params.search};
+  }
+
+  componentDidMount() {
+    axios
+      .get(`https://jobs.github.com/positions.json`)
+      .then(res => {
+        let info: [];
+        const arr = res.data;
+        for (let index = 0; index < arr.length; index += 1){
+            info.push(arr[index]);
+        } 
+
+        this.setState(() => ({
+          jobs: info
+        }));
+      })
+      .catch(err => console.log(err.message)); //eslint-disable-line
+  }
+
+  render() {
+    return (
+      <ScrollView style={{flex:1}}>
+        <Text>{this.state.search}</Text>
+        {this.state.jobs.map((data, index) => (
+          <Text key={index}>{data.title}</Text>
+        ))}
+        <Button
+          title="Go to Job Detail... again"
+          onPress={() => this.props.navigation.navigate("JobDetail")}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+class JobDetail extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Details Screen</Text>
-        <Button
-          title="Go to Details... again"
-          onPress={() => this.props.navigation.navigate("Home")}
-        />
       </View>
     );
   }
@@ -33,7 +81,8 @@ class DetailsScreen extends React.Component {
 const AppNavigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Details: DetailsScreen
+    ListOfJobs: ListOfJobs,
+    JobDetail: JobDetail
   },
   {
     initialRouteName: "Home"
@@ -46,7 +95,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
-  }
+  },
+  textInput: {
+    borderColor: 'blue',
+    borderWidth: 2
+  },
 });
 
 export default createAppContainer(AppNavigator);
